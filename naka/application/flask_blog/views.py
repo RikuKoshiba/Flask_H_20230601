08178006dcfcd28e48ce0f5d97daa8e79e682779
nakayuna27 <nakagawa.yuna.xa@mynavi.jp>
2023-06-01 07:16:13 +0000

    add: session管理

--- naka/application/flask_blog/views.py
+++ naka/application/flask_blog/views.py
@@ -1,8 +1,11 @@
 from flask import request, redirect, url_for, render_template, flash, session
 from flask_blog import app
 
+#/が渡されたら、show_entries()
 @app.route('/')
 def show_entries():
+    if not session.get('logged_in'):
+       return redirect('/login')
     return render_template('entries/index.html')
 
 @app.route('/login', methods = ['GET', 'POST'])
@@ -13,9 +16,11 @@ def login():
         elif request.form['password'] != app.config['PASSWORD']:
           print('パスワードが異なります')
         else:
+           session['logged_in'] = True
            return redirect('/')
     return render_template('login.html')
 
 # @app.route('logout')
 # def logout():
+#    session.pop('logged_in', None)
 #    return redirect('/')

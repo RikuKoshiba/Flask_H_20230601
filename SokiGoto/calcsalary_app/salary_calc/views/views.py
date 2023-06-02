6257069e09b0c880d42404cf9d510690651f4c52
SokiGoto57 <goto.soki.sj@mynavi.jp>
2023-06-02 04:39:01 +0000

    add flash new line

--- SokiGoto/calcsalary_app/salary_calc/views/views.py
+++ SokiGoto/calcsalary_app/salary_calc/views/views.py
@@ -2,11 +2,13 @@ from salary_calc import app
 from flask import request, redirect, url_for, render_template, flash, session
 from currency_converter import CurrencyConverter
 
+
 @app.route("/", methods=["GET", "POST"])
 def input():
     # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
     # sessionを使う方法
-    salary = session.pop("salary", None)
+    salary = session.pop("salary", "")
+    currency = session.pop("currency", "JPY")
     # ===========================================
     # sessionを使わない方法
     # salary = request.args.get("salary")
@@ -15,7 +17,8 @@ def input():
     currencies = curcon.currencies
     return render_template("input.html",
                            before_salary=salary,
-                           currencies=currencies)
+                           currencies=currencies,
+                           selected_currency=currency)
 
 
 @app.route("/output", methods=["GET", "POST"])
@@ -28,16 +31,20 @@ def output():
         return redirect(url_for("input"))
 
     input_salary = int(request.form["salary"])
-    new_cur = request.form["currency"]
-    # print(new_cur)
+    currency = request.form["currency"]
     curcon = CurrencyConverter()
-    salary = int(curcon.convert(input_salary, new_cur, 'JPY'))
+    salary = int(curcon.convert(input_salary, currency, 'JPY'))
 
     if salary > 9999999999:
-        flash("給与には最大9,999,999,999円まで入力可能です")
+        if currency != "JPY":
+            flash("給与には最大9,999,999,999円まで入力可能です。\n" +
+                  f"{input_salary:,}{currency}は{salary:,}円です。")
+        else:
+            flash("給与には最大9,999,999,999円まで入力可能です。")
         # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
         # sessionを使う方法
-        session["salary"] = salary
+        session["salary"] = input_salary
+        session["currency"] = currency
         return redirect(url_for("input"))
         # ===========================================
         # sessionを使わない方法

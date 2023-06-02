9095c74a831e76e434100dc48f3f45cbe776268c
SokiGoto57 <goto.soki.sj@mynavi.jp>
2023-06-02 06:35:55 +0000

    add sql

--- SokiGoto/application/flask_blog/__init__.py
+++ SokiGoto/application/flask_blog/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from datetime import timedelta
 import secrets
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config.from_object("flask_blog.config")
@@ -8,5 +9,6 @@ app.config["SECRET_KEY"] = secrets.token_hex()
 app.permanent_session_lifetime = \
     timedelta(minutes=app.config["SESSION_TIME_MIN"])
 
+db = SQLAlchemy(app)
 
 import flask_blog.views

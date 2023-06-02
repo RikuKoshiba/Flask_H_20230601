58d11c41ccfb6c8d145bc5f22713ed99f84d1aae
RikuKoshiba <koshiba.riku.ig@mynavi.jp>
2023-06-02 08:38:41 +0000

    202306021738

--- koshiba/calcsalary_app/salary/views/views.py
+++ koshiba/calcsalary_app/salary/views/views.py
@@ -4,14 +4,24 @@ from decimal import Decimal, ROUND_HALF_UP
 
 @app.route('/')
 def m_input():
-    # if request.method == 'POST':
-    #     return render_template('input.html')
     return render_template('input.html')
 
 @app.route('/output', methods=['GET', 'POST'])
 def m_output():
     if request.method == 'POST':
-        return render_template('output.html')
+        #給与で税率変更
+        allowance = int(request.form["input_salary"])
+        if allowance > 1000000:
+            tax = (allowance - 1000000) * 0.2 + 1000000 * 0.1
+        else:
+            tax = allowance * 0.1
+
+        # 税率から支給額算出
+        tax = Decimal(str(tax)).quantize(Decimal("0"),rounding=ROUND_HALF_UP)
+        salary = allowance - tax
+
+        # output.htmlに結果を渡す、遷移
+        return render_template('output.html', allowance=allowance, tax=tax, salary=salary)
 
 
 # @app.route('/logout')
@@ -19,20 +29,4 @@ def m_output():
 #     # session.pop('logged_in', None)
 #     flash('ログアウトしました')
 #     return redirect(url_for('show_entries'))
-
-# #給与で税率変更
-# @app.route('/calculate')
-# allowance = "{:,}".format(input_salary)
-# def calculation():
-#     if allowance > 1000000:
-#         tax = (allowance - 1000000) * 0.2 + 1000000 * 0.1
-#     else:
-#         tax = allowance * 0.1
-
-#     # 税率から支給額算出
-#     tax = Decimal(str(tax)).quantize(Decimal("0"),rounding=ROUND_HALF_UP)
-#     salary = allowance - tax
-
-#     # output.htmlに結果を渡す
-#     return render_template("output.html", allowance=allowance, tax=tax, salary=salary)
 

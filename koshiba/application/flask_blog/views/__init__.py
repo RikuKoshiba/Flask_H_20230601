f4993b08c917a099fbb72211d8d6524164ab00b7
RikuKoshiba <koshiba.riku.ig@mynavi.jp>
2023-06-05 04:02:12 +0000

    202306051302

--- koshiba/application/flask_blog/views/__init__.py
+++ koshiba/application/flask_blog/views/__init__.py
@@ -1,9 +1,9 @@
-from flask  import Flask
-from flask_sqlalchemy import SQLAlchemy
+# from flask  import Flask
+# from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
-app.config.from_object('flask_blog.config')
+# app = Flask(__name__)
+# app.config.from_object('flask_blog.config')
 
-db = SQLAlchemy(app)
+# db = SQLAlchemy(app)
 
-from flask_blog.views import views, entries
+# from flask_blog.views import views, entries

2fbcc9f5e3e5585602b35c3102efabf9f105206f
RikuKoshiba <koshiba.riku.ig@mynavi.jp>
2023-06-07 08:38:10 +0000

    202306071738

--- koshiba/holiday_app/holiday/views/input.py
+++ koshiba/holiday_app/holiday/views/input.py
@@ -1,7 +1,6 @@
-from flask import render_template
-# from flask import request, redirect, url_for, render_template, flash, session
+from flask import request, redirect, url_for, render_template, flash, session
 from holiday import app
-# from holiday import db
+from holiday import db
 from holiday.models.mst_holiday import Holiday
 
 @app.route('/')
--- koshiba/holiday_app/holiday/views/list.py
+++ koshiba/holiday_app/holiday/views/list.py
@@ -6,6 +6,6 @@ from holiday.models.mst_holiday import Holiday
 @app.route('/list', methods=['GET', 'POST'])
 def m_list():
     if request.method == 'POST':
-        return render_template('list.html')
-    
-
+        holidaies = Holiday.query.order_by(Holiday.holi_date.desc()).all()
+        return render_template('list.html', holidaies=holidaies)
+        

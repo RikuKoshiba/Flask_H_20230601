f3b2b787616c6621d42185411e9feb2298ab5673
RikuKoshiba <koshiba.riku.ig@mynavi.jp>
2023-06-07 04:05:43 +0000

    202306071305

--- koshiba/holiday_app/holiday/models/mst_holiday.py
+++ koshiba/holiday_app/holiday/models/mst_holiday.py
@@ -1,14 +1,14 @@
 from holiday import db
-from datetime import datetime
+from datetime import date
 
 class Holiday(db.Model):
     __tablename__ = 'holiday'
     holi_date = db.Column(db.Date, primary_key = True)
     holi_text = db.Column(db.String(20))
 
-    def __init__(self, date=None, text=None):
-        self.date = date
-        self.text = text
+    def __init__(self, holi_date=None, holi_text=None):
+        self.holi_date = holi_date
+        self.holi_text = holi_text
 
     def __repr__(self):
         return '<Holiday date:{} text:{}>'.format(self.date, self.text)
--- koshiba/holiday_app/holiday/views/maintenance_date.py
+++ koshiba/holiday_app/holiday/views/maintenance_date.py
@@ -7,5 +7,28 @@ from holiday.models.mst_holiday import Holiday
 @app.route('/result', methods=['GET', 'POST'])
 def m_result():
     if request.method == 'POST':
+        # 登録、更新、削除のデータベース処理、valueによって条件分岐を行う
+        if request.form["button"] == "insert_update": # 登録・更新の場合
+                holiday = Holiday(
+                    holi_date=request.form['holiday'],
+                    holi_text=request.form['holiday_text']
+                )
+                db.session.add(holiday)
+                db.session.commit()
+        elif request.form["button"] == "delate":
+                holiday = Holiday.query.get()
+                db.session.delete(holiday)
+                db.session.commit()
         return render_template('result.html')
 
+# # 登録するデータの編集
+# holiday = Holiday(
+#     holi_date = date(2024, 1, 1),
+#     holi_text = "お正月"
+# )
+
+# # INSERT処理
+# session.add(holiday)
+
+# # コミット
+# session.commit()
